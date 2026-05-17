5a9042a552c571b37d69fc79065b4223bae8a282
Matt Poling <matt.poling@gmail.com>
2026-05-17 13:04:25 -0700

    feat(vacationhub): add brand chip filter with URL state

--- vacationhub/home.js
+++ vacationhub/home.js
@@ -14,6 +14,35 @@
 
   renderHero(index);
 
+  let currentBrand = VH.params().get('brand') || 'all';
+  renderChips(index, currentBrand);
+
+  function renderChips(idx, active) {
+    const chips = $('#chips');
+    const make = (slug, label) =>
+      el('button', {
+        class: 'chip' + (slug === active ? ' active' : ''),
+        'data-brand': slug,
+        'aria-pressed': slug === active ? 'true' : 'false',
+        onclick: () => selectBrand(slug),
+      }, [label]);
+
+    chips.replaceChildren(
+      make('all', 'All'),
+      ...idx.brands.map((b) => make(b.slug, b.name)),
+    );
+  }
+
+  function selectBrand(slug) {
+    currentBrand = slug;
+    const url = new URL(location.href);
+    if (slug === 'all') url.searchParams.delete('brand');
+    else url.searchParams.set('brand', slug);
+    history.replaceState(null, '', url);
+    renderChips(index, slug);
+    if (VH.refilterRails) VH.refilterRails(slug);   // Task 8 will define this
+  }
+
   function renderHero(idx) {
     const featuredSlug = idx.home.featured;
     const park = idx.parks.find((p) => p.slug === featuredSlug);
